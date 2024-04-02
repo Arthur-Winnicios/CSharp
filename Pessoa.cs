3aaebbbf7efce41bfcd0484402573074aff3f8a3
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-04-02 16:49:43 -0400

    adicionado exemplo de classe funcionario

--- Pessoa.cs
+++ Pessoa.cs
@@ -36,3 +36,22 @@
             set { curso = value; }
         }
     }
+    public class Funcionario : Pessoa {
+        private double salario;
+
+        //Método para calcular o bônus
+        public double Bonus() {
+            return salario * 0.1; //10% de bônus padrão sobre o salário
+        }
+
+        //Construtor da classe Funcionario
+        public Funcionario(string nome, int idade, string cpf, double salario) : base(nome, idade, cpf) {
+            Salario = salario;
+        }
+
+        //Propriedades da classe Funcionario que permite acesso controlado a um campo privado
+        public string Salario {
+            get { return salario; }
+            set { salario = value; }
+        }
+    }
